4839411af0e97aeef9371ae90298fa39fc47350c
Dimitris Sparagis <dimsparagis@gmail.com>
2025-01-12 16:06:25 +0200

    Redeployed the contract

--- src/Crowdfunding.js
+++ src/Crowdfunding.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import abi from './Crowdfunding.json';
 
-const address = '0x9Eec08dC82B3B17CD5bAb73E9c19c926f77bDDB8';
+const address = '0xAE588629e2Fc9912c9928F1a9d92d6942ad265C5';
 const contract = new web3.eth.Contract(abi, address);
 
 export default contract;
