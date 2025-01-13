f6249400f1ce35ff82521ac8da871f25413489e9
Dimitris Sparagis <dimsparagis@gmail.com>
2025-01-13 17:18:17 +0200

    Updates

--- src/Crowdfunding.js
+++
@@ -1,7 +0,0 @@
-import web3 from './web3';
-import abi from './Crowdfunding.json';
-
-const address = '0xAE588629e2Fc9912c9928F1a9d92d6942ad265C5';
-const contract = new web3.eth.Contract(abi, address);
-
-export default contract;
