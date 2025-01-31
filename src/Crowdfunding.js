14c7871c7b61491616b4b67f363e51a7e482696a
Dimitris Sparagis <dimsparagis@gmail.com>
2025-01-31 12:05:14 +0200

    Changed abi and redeployed

--- src/Crowdfunding.js
+++ src/Crowdfunding.js
@@ -1,7 +1,8 @@
 import web3 from './web3';
 import abi from './Crowdfunding.json';
 
-const address = '0xAE588629e2Fc9912c9928F1a9d92d6942ad265C5';
+const address = '0x261F9e56bbCD51291d00fCb991f0B7Eef5d00418';
 const contract = new web3.eth.Contract(abi, address);
+
 
 export default contract;
